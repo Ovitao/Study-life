9e70a143192af0d55e758654be6e7b57809beded
Ovitao <72672986+Ovitao@users.noreply.github.com>
2023-08-29 14:07:09 +0300

    Add files via upload

--- calendar_with_notes/calendar.h
+++ calendar_with_notes/calendar.h
@@ -1,4 +1,14 @@
 #pragma once
+
+
+/*
+* Calendar With Notes:
+This calendar hold 2023-2025 years;
+current date marked as red;
+date with note marked as cyan
+Exit with Exit button to save notes to the file "notes.txt"
+
+*/
 #include "fltk.h"
 #include <map>
 #include <vector>
