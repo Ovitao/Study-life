0489fb9fa14f4329c7fbdeaf01cb63c61428d2d3
Ovitao <72672986+Ovitao@users.noreply.github.com>
2023-08-29 14:07:44 +0300

    Update calendar.h

--- calendar_with_notes/calendar.h
+++ calendar_with_notes/calendar.h
@@ -3,7 +3,7 @@
 
 /*
 * Calendar With Notes:
-This calendar hold 2023-2025 years;
+This calendar holds 2023-2025 years;
 current date marked as red;
 date with note marked as cyan
 Exit with Exit button to save notes to the file "notes.txt"
